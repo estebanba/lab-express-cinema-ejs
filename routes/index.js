e41b1785486a62312ae2c906cce4f84e2027871f
estebanba <esteban.ba@gmail.com>
2022-05-27 15:04:24 +0200

    iteration 3 done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+// Connects to MongoDB
+require('../db')
+
+// Requests out model
+const Movie = require("../models/Movie.model");
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
-router.get('/movies', (req, res, next) => res.render('movies'));
+router.get('/movies', async (req, res, next) => {
+    let allMovies = await Movie.find()
+    console.log(allMovies);
+    res.render('movies', { allMovies })
+})
 
 module.exports = router;
