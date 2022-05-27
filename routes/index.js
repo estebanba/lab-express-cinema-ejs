02bf694308ed23e6463231cf1cff122913c44c85
estebanba <esteban.ba@gmail.com>
2022-05-27 17:01:39 +0200

    iteration 4 Done

--- routes/index.js
+++ routes/index.js
@@ -12,15 +12,15 @@ router.get('/', (req, res, next) => res.render('index'));
 
 router.get('/movies', async (req, res, next) => {
     let allMovies = await Movie.find()
-    console.log(allMovies);
+    // console.log(allMovies);
     res.render('movies', { allMovies })
 });
 
 router.get('/movies/:id', async (req, res, next) => {
-    let allMovies = await Movie.find()
-    let data = res.send(req.params['id'])
-    let movie = allMovies[data]
-    res.render('oneMovie', { movie })
+    const movieId = req.params.id
+    let specificMovie = await Movie.findById(movieId)
+    console.log(specificMovie)
+    res.render('oneMovie', { specificMovie })
 });
 
 module.exports = router;
